40214d230eb504981563acb0aa35a8e41efc51f8
jose <jl.rodriguez@iesdoctorbalmis.com>
2021-11-04 15:01:38 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/Superheroe.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes
+{
+    class Superheroe
+    {
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string imagen;
+
+        public string Imagen
+        {
+            get { return imagen; }
+            set { imagen = value; }
+        }
+
+        private bool vengador;
+
+        public bool Vengador
+        {
+            get { return vengador; }
+            set { vengador = value; }
+        }
+
+        private bool xmen;
+
+        public bool Xmen
+        {
+            get { return xmen; }
+            set { xmen = value; }
+        }
+
+        private int heroe;
+
+        public int Heroe
+        {
+            get { return heroe; }
+            set { heroe = value; }
+        }
+
+
+
+
+
+    }
+}
